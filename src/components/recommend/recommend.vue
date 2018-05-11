a6e2657e8ab9d25cfa02b7fe7c995f8f868374f8
ylz <653816757@qq.com>
2018-05-11 17:20:00 +0800

    推荐页面全部完成，开始写歌手页面

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,50 +1,58 @@
 <template>
     <div class="recommend" ref="recommend">
-      <div class="recommend-content">
-        <div v-if="recommends.length" class="slider-wrapper">
-          <slider>
-            <div v-for="item in recommends">
-              <a :href="item.linkUrl"><img class="needsclick" :src="item.picUrl"></a>
-            </div>
-          </slider>
-
-        </div>
-        <div class="recommend-list">
-          <h1 class="list-title">热门歌单推荐</h1>
-          <ul>
-            <li v-for="item in discList">
-              <div class="icon">
-                <img width="60px" height="60px" :src="item.imgurl" alt="">
+      <scroll ref="scroll" class="recommend-content" :data="discList">
+        <div>
+          <div v-if="recommends.length" class="slider-wrapper">
+            <slider>
+              <div v-for="item in recommends">
+                <a :href="item.linkUrl"><img @load="loadImage" class="needsclick" :src="item.picUrl"></a>
               </div>
-              <div class="text">
-                <h2 class="name" v-html="item.creator.naem"></h2>
-                <p class="desc" v-html="item.dissname"></p>
-              </div>
-            </li>
-          </ul>
+            </slider>
+          </div>
+          <div class="recommend-list">
+            <h1 class="list-title">热门歌单推荐</h1>
+            <ul>
+              <li class="item" v-for="item in discList">
+                <div class="icon">
+                  <img width="60px" height="60px" v-lazy="item.imgurl" alt="">
+                </div>
+                <div class="text">
+                  <h2 class="name" v-html="item.creator.name"></h2>
+                  <p class="desc" v-html="item.dissname"></p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+        <div class="loading-container" v-show="!discList.length">
+          <loading></loading>
+        </div>
+      </scroll>
     </div>
 </template>
 
 <script>
-  // import Scroll from '@/base/scroll/scroll'
+  import Scroll from '@/base/scroll/scroll'
   import Slider from '@/base/slider/slider'
+  import Loading from '@/base/loading/loading'
   import {getRecommend,getDiscList} from "../../api/recommend";
   import {ERR_OK} from "../../api/config";
 
   export default {
     data(){
       return {
-        recommends:'',
-        discList:''
+        recommends:[],
+        discList:[]
       }
     },
     name: "recommend",
     created(){
       document.getElementsByTagName('body')[0].style.margin='0';
       this._getRecommend();
-      this._getDiscList();
+      setTimeout(()=>{
+        this._getDiscList();
+      },2000)
+      // this._getDiscList();
     },
     methods:{
       _getRecommend(){
@@ -60,11 +68,18 @@
           this.discList = res.data.list
         }
         })
+      },
+      loadImage(){
+        if(!this.checkLoaded){
+          this.$refs.scroll.refresh();
+          this.checkLoaded = true;
+        }
       }
     },
     components:{
       Slider,
-      // Scroll
+      Scroll,
+      Loading
     }
     }
 </script>
@@ -85,6 +100,9 @@
         width: 100%
         overflow: hidden
       .recommend-list
+        ul{
+          padding-left: 10px;
+        }
         .list-title
           height: 65px
           line-height: 65px
@@ -107,12 +125,13 @@
             flex: 1
             line-height: 20px
             overflow: hidden
-            font-size: $font-size-medium
+            font-size: 14px
             .name
-              margin-bottom: 10px
-              color: $color-text
+              margin-bottom: 4px
+              color: #333333
+              font-size: 16px;
             .desc
-              color: $color-text-d
+              color:#333333
       .loading-container
         position: absolute
         width: 100%
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,10 +1,14 @@
 <template>
-    <div>this is a singger page</div>
+    <div class="singer">this is a singger page</div>
 </template>
 
 <script>
   import {getSinger} from "../../api/singer";
   import {ERR_OK} from "../../api/config";
+  import Singer from '@/common/js/singer'
+
+  const HOT_NAME= '热门';
+  const HOT_SINGER_LENGTH = 10;
 
   export default {
     name: "singer",
@@ -20,15 +24,62 @@
       _getSinger(){
         getSinger().then((res)=>{
           if (res.code==ERR_OK){
-            console.log(res);
-            this.singerData = res;
+            console.log(this._normalizeSinger(res.data.list));
+            this.singerData = res.data.list;
           }
         })
+      },
+      _normalizeSinger(list){
+        let map = {
+          hot:{
+            title:HOT_NAME,
+            items:[]
+          }
+        }
+        list.forEach((item,index)=>{
+          if(index<HOT_SINGER_LENGTH){
+            map.hot.items.push(new Singer({
+              id:item.Fsinger_mid,
+              name:item.Fsinger_name
+            }));
+          }
+          const key = item.Findex;
+          if(!map[key]){
+            map[key] = {
+              title:key,
+              items:[]
+            }
+          }
+          map[key].items.push(new Singer({
+            id:item.Fsinger_mid,
+            name:item.Fsinger_name
+          }))
+        })
+        //为了得到有序列表，需要处理map
+        let hot = []
+        let ret = []
+        for (let key in map){
+          let val = map[key];
+          if(val.title.match(/[a-zA-z]/)){
+            ret.push(val)
+          }else if(val.title==HOT_NAME){
+            hot.push(val)
+          }
+        }
+        ret.sort((a,b)=>{
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
       }
     }
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer{
+    position: fixed
+    top: 88px
+    bottom:0
+    width: 100%
+  }
 </style>
